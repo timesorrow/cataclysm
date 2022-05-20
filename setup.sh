d15b360c489bb5b9839dde2be51ba34d23fcdb01
timesorrow <50745419+timesorrow@users.noreply.github.com>
2022-05-20 13:19:07 +0200

    s

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 apt update && apt upgrade -y
-apt-get -y install sudo
-adduser mangos
+apt-get -y install sudo nano 
+useradd mangos -p mangos -m
 usermod -aG sudo mangos
 su mangos
 sudo apt install git make cmake libssl-dev libbz2-dev build-essential default-libmysqlclient-dev libace-6.4.5 libace-dev python
